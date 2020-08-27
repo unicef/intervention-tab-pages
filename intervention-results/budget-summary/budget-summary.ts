373888cc0ad09fa89c1dfe34369dd0113426ec4b
lajos <kovago.lajos91@gmail.com>
2020-08-27 13:18:44 +0300

    added warnings to component

--- intervention-results/budget-summary/budget-summary.ts
+++ intervention-results/budget-summary/budget-summary.ts
@@ -8,18 +8,25 @@ import {connect} from 'pwa-helpers/connect-mixin';
 import {getStore} from '../../utils/redux-store-access';
 import {pageIsNotCurrentlyActive} from '../../utils/common-methods';
 import get from 'lodash-es/get';
+import FrNumbersConsistencyMixin from '../../common/mixins/fr-numbers-consistency-mixin';
+import '@unicef-polymer/etools-info-tooltip/etools-info-tooltip';
+import {frWarningsStyles} from '../../common/styles/fr-warnings-styles';
+import {isEmptyObject} from '../../utils/utils';
+import {Fr, Intervention, FrsDetails} from '../../common/models/intervention.types';
+import {customIcons} from '../../common/styles/custom-icons';
 
 /**
  * @customElement
  */
 @customElement('budget-summary')
-export class BudgetSummaryEl extends connect(getStore())(LitElement) {
+export class BudgetSummaryEl extends connect(getStore())(FrNumbersConsistencyMixin(LitElement)) {
   static get styles() {
-    return [gridLayoutStylesLit, elevationStyles];
+    return [gridLayoutStylesLit, elevationStyles, frWarningsStyles];
   }
   render() {
     // language=HTML
     return html`
+      ${customIcons}
       <style>
         ${sharedStyles} :host {
           display: block;
@@ -48,7 +55,7 @@ export class BudgetSummaryEl extends connect(getStore())(LitElement) {
               <label class="paper-label">Total CSO Contrib</label>
             </span>
           </div>
-          <div class="col col-1">
+          <div class="col col-2">
             <span>
               <label class="paper-label">Total Unicef Contrib</label>
             </span>
@@ -77,6 +84,23 @@ export class BudgetSummaryEl extends connect(getStore())(LitElement) {
         <div class="layout-horizontal">
           <div class="col col-1">
             <span>
+              <etools-info-tooltip
+                class="fr-nr-warn currency-mismatch"
+                icon-first
+                custom-icon
+                ?hide-tooltip="${this.allCurrenciesMatch(
+                  this.frsDetails.currencies_match,
+                  this.frsDetails.frs,
+                  this.budgetSummary.currency
+                )}"
+              >
+                <iron-icon icon="pmp-custom-icons:not-equal" slot="custom-icon"></iron-icon>
+                <span slot="message">
+                  ${this.getFrCurrencyTooltipMsg()}
+                </span>
+              </etools-info-tooltip>
+            </span>
+            <span>
               <label class="input-label" ?empty="${!this.budgetSummary.currency}">
                 ${this.budgetSummary.currency}
               </label>
@@ -85,35 +109,46 @@ export class BudgetSummaryEl extends connect(getStore())(LitElement) {
           <div class="col col-1">
             <span>
               <label class="input-label" ?empty="${!this.budgetSummary.hq_support_cost}">
-                ${this.budgetSummary.hq_support_cost} %
+                ${this.budgetSummary.hq_support_cost}
               </label>
             </span>
           </div>
           <div class="col col-2">
             <span>
               <label class="input-label" ?empty="${!this.budgetSummary.prgm_effectiveness}">
-                ${this.budgetSummary.prgm_effectiveness} %
+                ${this.budgetSummary.prgm_effectiveness}
               </label>
             </span>
           </div>
           <div class="col col-1">
             <span>
               <label class="input-label" ?empty="${!this.budgetSummary.partner_contribution_local}">
-                ${this.budgetSummary.partner_contribution_local}
+                ${this.displayCurrencyAmount(this.budgetSummary.partner_contribution_local, '0.00')}
               </label>
             </span>
           </div>
-          <div class="col col-1">
+          <div class="col col-2">
+            <etools-info-tooltip
+              class="fr-nr-warn currency-mismatch"
+              icon-first
+              custom-icon
+              ?hide-tooltip="${!this.frsConsistencyWarningIsActive(this._frsConsistencyWarning)}"
+            >
+              <iron-icon icon="pmp-custom-icons:not-equal" slot="custom-icon"></iron-icon>
+              <span slot="message">
+                ${this._frsConsistencyWarning}
+              </span>
+            </etools-info-tooltip>
             <span>
               <label class="input-label" ?empty="${!this.budgetSummary.unicef_cash_local}">
-                ${this.budgetSummary.unicef_cash_local}
+                ${this.displayCurrencyAmount(this.budgetSummary.unicef_cash_local, '0.00')}
               </label>
             </span>
           </div>
           <div class="col col-1">
             <span>
               <label class="input-label" ?empty="${this.budgetSummary.total_supply}">
-                ${this.budgetSummary.total_supply}
+                ${this.displayCurrencyAmount(this.budgetSummary.total_supply, '0.00')}
               </label>
             </span>
           </div>
@@ -146,16 +181,38 @@ export class BudgetSummaryEl extends connect(getStore())(LitElement) {
   @property({type: Object})
   budgetSummary!: BudgetSummary;
 
+  @property({type: Object})
+  intervention!: Intervention;
+
+  @property({type: String})
+  _frsConsistencyWarning = '';
+
+  @property({type: Object})
+  frsDetails!: FrsDetails;
+
   connectedCallback() {
     super.connectedCallback();
   }
 
   public stateChanged(state: any) {
+    if (!state.interventions.current) {
+      return;
+    }
     if (pageIsNotCurrentlyActive(get(state, 'app.routeDetails'), 'interventions', 'results')) {
       return;
     }
-    if (state.interventions.current) {
+    if (state.interventions.current && this.intervention.frs_details) {
       this.budgetSummary = selectBudgetSummary(state);
+      this.intervention = state.interventions.current;
+      this.frsDetails = this.intervention.frs_details;
+      const warn = this.checkFrsAndUnicefCashAmountsConsistency(
+        this.budgetSummary.unicef_cash_local!,
+        this.frsDetails.total_frs_amt,
+        this.intervention,
+        'interventionDetails',
+        true
+      );
+      this._frsConsistencyWarning = String(warn);
     }
   }
 }
