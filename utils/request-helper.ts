bff91218046200001cc301c08a183aff16131a68
Adriana Trif <trif.corina.adriana@gmail.com>
2020-09-11 14:43:06 +0300

    Fix import

--- utils/request-helper.ts
+++ utils/request-helper.ts
@@ -1,6 +1,6 @@
 import {sendRequest, EtoolsRequestConfig} from '@unicef-polymer/etools-ajax/etools-ajax-request';
 import {formatServerErrorAsText} from '@unicef-polymer/etools-ajax/ajax-error-parser';
-import {fireEvent} from '../../../../utils/fire-custom-event';
+import {fireEvent} from './fire-custom-event';
 
 export const _sendRequest = (etoolsReqConfig: EtoolsRequestConfig, _requestKey?: string, _checkProgress?: boolean) => {
   return sendRequest(etoolsReqConfig, _requestKey, _checkProgress)
