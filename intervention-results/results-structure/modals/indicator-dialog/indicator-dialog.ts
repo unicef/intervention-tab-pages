70c197825a1969dad5897b8038d9253430b302cf
laza88 <lazaryandrei@gmail.com>
2020-09-14 08:24:02 +0300

    code adjustments

--- intervention-results/results-structure/modals/indicator-dialog/indicator-dialog.ts
+++ intervention-results/results-structure/modals/indicator-dialog/indicator-dialog.ts
@@ -366,6 +366,6 @@ export class IndicatorDialog extends IndicatorDialogTabsMixin(SaveIndicatorMixin
   }
 
   _hideAddDisaggreations(isCluster: boolean, currentUser: User) {
-    return isCluster || !currentUser.is_unicef_user;
+    return isCluster || !userIsPme(currentUser) || !currentUser.is_unicef_user;
   }
 }
