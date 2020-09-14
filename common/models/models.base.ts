d46e09df92417b7f81fb379444078112f4b3684c
Uladzimir <v_urbanovich@mail.ru>
2020-09-14 14:58:10 +0300

    [ch22151] Connect Efective-efficient mgmt with backend

--- common/models/models.base.ts
+++ common/models/models.base.ts
@@ -1,9 +1,9 @@
 import pick from 'lodash-es/pick';
-import {Intervention, InterventionPermissionsFields, PlannedBudget} from './intervention.types';
+import {Intervention, InterventionPermissionsFields, ManagementBudget, PlannedBudget} from './intervention.types';
 import {AnyObject} from './globals.types';
 
 export class ModelsBase {
-  setObjProperties(dataSource: Intervention | InterventionPermissionsFields | PlannedBudget) {
+  setObjProperties(dataSource: Intervention | InterventionPermissionsFields | PlannedBudget | ManagementBudget) {
     Object.assign(this, pick(dataSource, Object.keys(this as AnyObject)));
   }
   setObjProperty(propKey: string, propValue: AnyObject) {
