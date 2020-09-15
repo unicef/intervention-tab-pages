d67546e15152e087d45765378aafb0c3fa49d802
dcioara <dan.cioara@nordlogic.com>
2020-09-15 10:24:49 +0300

    [ch22405] Remove constants from config/config file

--- common/config.ts
+++
@@ -1,6 +0,0 @@
-export const ROOT_PATH = '/' + getBasePath().replace(window.location.origin, '').slice(1, -1) + '/';
-
-function getBasePath() {
-  console.log(document.getElementsByTagName('base')[0].href);
-  return document.getElementsByTagName('base')[0].href;
-}
