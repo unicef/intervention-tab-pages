e4bf1c39b9025028698198cf4137846e6974efb2
lajos <kovago.lajos91@gmail.com>
2020-09-16 10:39:27 +0300

    tsc warning fixes

--- common/models/intervention.types.ts
+++ common/models/intervention.types.ts
@@ -125,7 +125,6 @@ export class InterventionSupplyItem {
   total_price: number | undefined = undefined;
   other_mentions = '';
   intervention = '';
-  outputs: string[] = [];
 }
 
 export class InterventionPermissionsFields {
