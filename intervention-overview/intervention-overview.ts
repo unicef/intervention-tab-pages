2ac1b505a8e1f6cfbebcf8c4015a72d15b3ca2fe
Adriana Trif <trif.corina.adriana@gmail.com>
2020-10-08 13:59:26 +0300

    Update Overview tab %total calulation& formatting

--- intervention-overview/intervention-overview.ts
+++ intervention-overview/intervention-overview.ts
@@ -19,6 +19,7 @@ import {pageIsNotCurrentlyActive} from '../utils/common-methods';
 import {AnyObject, RootState} from '../common/models/globals.types';
 import {fireEvent} from '../utils/fire-custom-event';
 import {StaticPartner} from '../common/models/partner.types';
+import {result} from 'lodash-es';
 
 /**
  * @customElement
@@ -167,7 +168,7 @@ export class InterventionOverview extends connect(getStore())(LitElement) {
               <label class="label-secondary-color">
                 % Total value of Unicef's contribution that is Effective and Efficient Programme Management Cost
               </label>
-              <div class="input-label">${this.getUnicefEEContribOutOfTotalEE()}</div>
+              <div class="input-label">${this.getUnicefEEContribOutOfTotaUnicefContrib()}</div>
             </div>
           </div>
         </div>
@@ -335,14 +336,40 @@ export class InterventionOverview extends connect(getStore())(LitElement) {
     });
   }
 
-  getUnicefEEContribOutOfTotalEE() {
+  getUnicefEEContribOutOfTotaUnicefContrib() {
     const totalEEUnicefContrib = this.getUnicefEEContrib(this.intervention.management_budgets);
-    const totalEE = this.intervention.management_budgets ? this.intervention.management_budgets.total : 0;
-    const percentage = Math.round((totalEEUnicefContrib * 100) / (totalEE | 1)).toFixed(2);
-    return percentage + ' %';
+    const rawPercentage = (totalEEUnicefContrib * 100) / (this.getTotalUnicefContrib() || 1);
+
+    return this.formatPercentage(rawPercentage) + '%';
   }
 
-  getUnicefEEContrib(management_budgets?: ManagementBudget) {
+  formatPercentage(rawPercentage: number) {
+    let percentage = '0';
+    if (rawPercentage === 0) {
+      return percentage;
+    }
+    if (rawPercentage < 0.01) {
+      percentage = rawPercentage.toFixed(4); // Taking into consideration values like 0.0018
+    } else {
+      percentage = rawPercentage.toFixed(2);
+    }
+    if (this.decimalFractionEquals0(percentage)) {
+      percentage = percentage.substring(0, percentage.lastIndexOf('.')); // Removing `.00` form value like `100.00%`
+    }
+    return percentage;
+  }
+
+  private decimalFractionEquals0(percentage: string) {
+    return percentage.lastIndexOf('.') > 0 && Number(percentage.substring(percentage.lastIndexOf('.') + 1)) === 0;
+  }
+
+  private getTotalUnicefContrib() {
+    return Number(
+      this.intervention.planned_budget ? this.intervention.planned_budget.total_unicef_contribution_local || 0 : 0
+    );
+  }
+
+  private getUnicefEEContrib(management_budgets?: ManagementBudget) {
     if (!management_budgets) {
       return 0;
     }
