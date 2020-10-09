7a2e65d11689206b4eed4e55d832852ca709ff8e
lajos <kovago.lajos91@gmail.com>
2020-10-09 10:56:17 +0300

    updated css classes

--- intervention-overview/intervention-overview.ts
+++ intervention-overview/intervention-overview.ts
@@ -148,7 +148,7 @@ export class InterventionOverview extends connect(getStore())(LitElement) {
 
         <div class="row-h flex-c">
           <div class="col col-4">
-            <div class="inline-left">
+            <div class="inline">
               <label class="label-secondary-color">Total value of the Effective Programme management Cost</label>
               <etools-currency-amount-input
                 class="w100"
@@ -163,7 +163,7 @@ export class InterventionOverview extends connect(getStore())(LitElement) {
           </div>
 
           <div class="col col-6">
-            <div class="inline-left">
+            <div class="inline">
               <label class="label-secondary-color">
                 % Total value of Unicef's contribution that is Effective and Efficient Programme Management Cost
               </label>
@@ -174,7 +174,7 @@ export class InterventionOverview extends connect(getStore())(LitElement) {
 
         <div class="row-h flex-c">
           <div class="col col-4">
-            <div class="inline-left">
+            <div class="inline">
               <label class="label-secondary-color">Unicef Cash Contribution</label>
               <etools-currency-amount-input
                 .value="${this.intervention.planned_budget.unicef_cash_local}"
@@ -187,7 +187,7 @@ export class InterventionOverview extends connect(getStore())(LitElement) {
             </div>
           </div>
           <div class="col col-4">
-            <div class="inline-left">
+            <div class="inline">
               <label class="label-secondary-color">Unicef Supply Contribution</label>
               <etools-currency-amount-input
                 .value="${this.intervention.planned_budget.in_kind_amount_local}"
@@ -200,7 +200,7 @@ export class InterventionOverview extends connect(getStore())(LitElement) {
             </div>
           </div>
           <div class="col col-4">
-            <div class="inline-left">
+            <div class="inline">
               <label class="label-secondary-color">Total Unicef Contribution</label>
               <etools-currency-amount-input
                 .value="${this.intervention.planned_budget.total_unicef_contribution_local}"
