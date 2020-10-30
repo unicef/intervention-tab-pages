459af31c8c0549ce2a9849e161d33a98596d29e1
dcioara <dan.cioara@nordlogic.com>
2020-10-30 15:14:36 +0200

    adjustments

--- common/selectors.ts
+++ common/selectors.ts
@@ -3,7 +3,7 @@ import {Intervention} from '@unicef-polymer/etools-types';
 
 export const currentSubpage = (state: any) => state.app!.routeDetails?.subRouteName;
 export const currentPage = (state: any) => state.app!.routeDetails?.routeName;
-export const currentIntervention = (state: any) => state.interventions.current;
+export const currentIntervention = (state: any) => state.interventions?.current;
 export const currentInterventionPermissions = (state: any) => state.interventions.current.permissions;
 export const currentInterventionPlannedBudget = (state: any) => state.interventions.current.planned_budget;
 export const isUnicefUser = (state: any) => state.user?.data?.is_unicef_user;
