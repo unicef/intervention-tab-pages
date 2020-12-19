a488f1e788b56251be0f00446d100bcdd9ae1e94
Adriana Trif <trif.corina.adriana@gmail.com>
2020-12-19 13:01:48 +0200

    clean up

--- common/styles/shared-styles-polymer.ts
+++
@@ -1,9 +0,0 @@
-import {sharedStylesContent} from './shared-styles-lit';
-
-export const sharedStylesPolymer = () => {
-  const template = document.createElement('template');
-  template.innerHTML = `<style>
-    ${sharedStylesContent}
-   </style>`;
-  return template;
-};
