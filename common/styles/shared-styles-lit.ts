8b94298b74883ea27f9c95e50a9b7ec69a8b6791
lajos <kovago.lajos91@gmail.com>
2021-01-07 15:41:02 +0200

    do not show shadow on lcick on elemenet

--- common/styles/shared-styles-lit.ts
+++ common/styles/shared-styles-lit.ts
@@ -310,7 +310,7 @@ export const sharedStylesContent = `
     --iron-icon-height: 14px;
   }
 
-  *:focus:not(paper-icon-button):not(paper-radio-button):not(paper-checkbox):not(paper-toggle-button) {
+  *:focus:not(paper-icon-button):not(paper-radio-button):not(paper-checkbox):not(paper-toggle-button):not(paper-input):not(paper-textarea) {
     outline: 0;
     box-shadow: var(--paper-material-elevation-3_-_box-shadow);
   }
