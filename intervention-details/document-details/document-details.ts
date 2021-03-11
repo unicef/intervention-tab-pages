ee251fab419e68d51728eaae4f0344a66ec06c47
Adriana Trif <trif.corina.adriana@gmail.com>
2021-03-11 14:50:14 +0200

    PD Details Context add character limitation

--- intervention-details/document-details/document-details.ts
+++ intervention-details/document-details/document-details.ts
@@ -71,6 +71,7 @@ export class DocumentDetailsElement extends CommentsMixin(ComponentBaseMixin(Lit
             ?required="${this.permissions.required.title}"
             error-message="This field is required"
             maxlength="5000"
+            .charCounter="${!this.isReadonly(this.editMode, this.permissions.edit?.title)}"
           >
           </paper-textarea>
         </div>
@@ -84,9 +85,10 @@ export class DocumentDetailsElement extends CommentsMixin(ComponentBaseMixin(Lit
             placeholder="—"
             .value="${this.data.context}"
             @value-changed="${({detail}: CustomEvent) => this.valueChanged(detail, 'context')}"
-            ?readonly="${this.isReadonly(this.editMode, this.permissions.edit.context)}"
+            ?readonly="${this.isReadonly(this.editMode, this.permissions.edit?.context)}"
             ?required="${this.permissions.required.context}"
-            maxlength="5000"
+            maxlength="7000"
+            .charCounter="${!this.isReadonly(this.editMode, this.permissions.edit?.context)}"
           >
           </paper-textarea>
         </div>
@@ -99,9 +101,10 @@ export class DocumentDetailsElement extends CommentsMixin(ComponentBaseMixin(Lit
             placeholder="—"
             .value="${this.data.implementation_strategy}"
             @value-changed="${({detail}: CustomEvent) => this.valueChanged(detail, 'implementation_strategy')}"
-            ?readonly="${this.isReadonly(this.editMode, this.permissions.edit.implementation_strategy)}"
+            ?readonly="${this.isReadonly(this.editMode, this.permissions.edit?.implementation_strategy)}"
             ?required="${this.permissions.required.implementation_strategy}"
             maxlength="5000"
+            .charCounter="${!this.isReadonly(this.editMode, this.permissions.edit?.implementation_strategy)}"
           >
           </paper-textarea>
         </div>
@@ -114,9 +117,10 @@ export class DocumentDetailsElement extends CommentsMixin(ComponentBaseMixin(Lit
             placeholder="—"
             .value="${this.data.ip_program_contribution}"
             @value-changed="${({detail}: CustomEvent) => this.valueChanged(detail, 'ip_program_contribution')}"
-            ?readonly="${this.isReadonly(this.editMode, this.permissions.edit.ip_program_contribution)}"
+            ?readonly="${this.isReadonly(this.editMode, this.permissions.edit?.ip_program_contribution)}"
             ?required="${this.permissions.required.ip_program_contribution}"
             maxlength="5000"
+            .charCounter="${!this.isReadonly(this.editMode, this.permissions.edit?.ip_program_contribution)}"
           >
           </paper-textarea>
         </div>
