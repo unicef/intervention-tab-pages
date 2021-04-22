935629c9bb2a8182cfc8b49d7b3daf3dadc90713
Adriana Trif <trif.corina.adriana@gmail.com>
2021-04-22 18:45:31 +0300

    Clean up

--- intervention-tabs.ts
+++ intervention-tabs.ts
@@ -361,10 +361,10 @@ export class InterventionTabs extends connectStore(LitElement) {
       return;
     }
     this.tabChanged(newTabName, this.activeTab, newSubTab, this.activeSubTab);
-    this.handleIntermittent2TabsUnderlined(e.target as EtoolsTabs);    
+    this.fixIntermittent2TabsUnderlined(e.target as EtoolsTabs);    
   }
 
-  handleIntermittent2TabsUnderlined(etoolsTabs: EtoolsTabs) {
+  fixIntermittent2TabsUnderlined(etoolsTabs: EtoolsTabs) {
     if (this.activeSubTab) {
       setTimeout(() => etoolsTabs.notifyResize());
     }
