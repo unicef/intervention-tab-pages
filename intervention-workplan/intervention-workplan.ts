cb68b74453b4bb5352a157fc3fa20d9e1ab743f1
Adriana Trif <trif.corina.adriana@gmail.com>
2022-03-17 22:11:46 +0200

    Editor UI draft

--- intervention-workplan/intervention-workplan.ts
+++ intervention-workplan/intervention-workplan.ts
@@ -1,4 +1,4 @@
-import {LitElement, customElement, html} from 'lit-element';
+import {LitElement, customElement, html, property} from 'lit-element';
 import './budget-summary/budget-summary';
 import './supply-agreement/supply-agreement';
 import './results-structure/results-structure';
@@ -6,6 +6,11 @@ import './effective-efficient-programme-mgmt/effective-efficient-programme-mgmt'
 import './non-financial-contribution/non-financial-contribution';
 import './hq-contribution/hq-contribution';
 import {fireEvent} from '@unicef-polymer/etools-modules-common/dist/utils/fire-custom-event';
+
+// ----
+import '@polymer/paper-icon-button/paper-icon-button';
+import '@polymer/iron-icons';
+// ----
 
 /**
  * @customElement
@@ -15,14 +20,119 @@ export class InterventionWorkplan extends LitElement {
   render() {
     // language=HTML
     return html`
-      <style></style>
+      <style>
+        table {
+          width: 100%;
+          border-collapse: collapse;
+          background-color: white;
+        }
+        td {
+          border: 1px solid #a4a1a1;
+          vertical-align: top;
+          padding: 8px;
+        }
 
-      <budget-summary></budget-summary>
-      <results-structure></results-structure>
-      <effective-and-efficient-programme-management></effective-and-efficient-programme-management>
-      <hq-contribution></hq-contribution>
-      <supply-agreements></supply-agreements>
-      <non-financial-contribution></non-financial-contribution>
+        td.col-1 {
+          width: 50px;
+          text-align: center;
+          vertical-align: middle;
+          padding: 6px;
+        }
+        td.col-2 {
+          width: 50%;
+        }
+        td.col-g {
+          width: 12.5%;
+        }
+        td.col-6 {
+          width: 12.5%;
+          vertical-align: top;
+          text-align: right;
+        }
+        tr.edit > td {
+          height: 30px;
+          border-bottom: none;
+        }
+        tr.header > td {
+          font-weight: 600;
+          color: var(--secondary-text-color);
+          vertical-align: middle;
+          border-bottom: none;
+          border-top: none;
+        }
+        tr.text > td {
+          border-bottom: none;
+          border-top: none;
+        }
+
+        tr.add > td {
+          border-top: none;
+        }
+
+        td > div {
+          padding: 8px;
+        }
+
+        paper-icon-button[icon='add-box'] {
+          padding-left: 0;
+        }
+        paper-icon-button[icon='create'] {
+          padding-top: 0;
+        }
+
+        tr > td:first-of-type {
+          border-left: none;
+        }
+        tr > td:last-of-type {
+          border-right: none;
+        }
+
+        tr.blue {
+          background-color: #b6d5f1;
+        }
+        tr.gray-1 {
+          background-color: aliceblue;
+        }
+      </style>
+
+      <table>
+        <tbody>
+          <tr class="edit blue">
+            <td class="col-1"></td>
+            <td class="col-2"></td>
+            <td class="col-g"></td>
+            <td class="col-g"></td>
+            <td class="col-g"></td>
+            <td class="col-6">
+              <paper-icon-button icon="create" ?hidden="${this.readonly}"></paper-icon-button>
+            </td>
+          </tr>
+          <tr class="header blue">
+            <td>ID</td>
+            <td>Country Programme Output</td>
+            <td></td>
+            <td>CSO Contribution</td>
+            <td>UNICEF Cash</td>
+            <td>Total</td>
+          </tr>
+          <tr class="text blue">
+            <td>1.</td>
+            <td>Capacity of national system to scale upeequality nutrition- sensitive intervention</td>
+            <td></td>
+            <td>1,234.00</td>
+            <td>4,567.00</td>
+            <td>5,567.00</td>
+          </tr>
+          <tr class="add blue">
+            <td></td>
+            <td><paper-icon-button icon="add-box" ?hidden="${this.readonly}"></paper-icon-button> Add New PD Output</td>
+            <td class="col-g"></td>
+            <td class="col-g"></td>
+            <td class="col-g"></td>
+            <td class="col-6"></td>
+          </tr>
+        </tbody>
+      </table>
     `;
   }
 
@@ -34,4 +144,7 @@ export class InterventionWorkplan extends LitElement {
       loadingSource: 'interv-page'
     });
   }
+
+  @property({type: Boolean})
+  readonly = false;
 }
