6dfd78139b155e3a904300423d0cfa8e41c1e863
Adriana Trif <trif.corina.adriana@gmail.com>
2022-10-03 12:49:19 +0300

    Padd increase

--- intervention-workplan-editor/editor-utils/editor-table-styles.ts
+++ intervention-workplan-editor/editor-utils/editor-table-styles.ts
@@ -30,13 +30,13 @@ export const EditorTableStyles = css`
   }
   td.col-text {
     width: calc(47% - 85px);
-    padding-left: 24px;
+    padding-left: 25px;
   }
   td.a-item-padd {
-    padding-left: 24px;
+    padding-left: 25px;
   }
   td.a-item-add-padd {
-    padding-left: 14px;
+    padding-left: 15px;
   }
   td.col-g {
     width: 10%;
