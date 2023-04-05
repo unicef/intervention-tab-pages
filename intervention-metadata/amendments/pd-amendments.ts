a0831dfe274ebf963453c7709dd6dfc5e5bcb168
dcioara <dan.cioara@nordlogic.com>
2023-04-05 15:27:47 +0300

    [ch33478] Misalignements - agreement details, review tab, amendments

--- intervention-metadata/amendments/pd-amendments.ts
+++ intervention-metadata/amendments/pd-amendments.ts
@@ -101,6 +101,9 @@ export class PdAmendments extends CommentsMixin(LitElement) {
           flex: 1;
           max-width: 150px;
         }
+        .editable-row {
+          line-height: 24px;
+        }
       </style>
 
       <etools-content-panel show-expand-btn panel-title=${translate('AMENDMENTS')} comment-element="amendments">
