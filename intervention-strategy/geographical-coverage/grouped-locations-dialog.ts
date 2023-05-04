e886cca24c329908a550d1c0e6d3c802689d3b64
merceaemil <30448376+merceaemil@users.noreply.github.com>
2023-05-04 17:37:48 +0300

    [ch33638] Geographical Coverage: The Partner does not see the group location options (#974)

--- intervention-strategy/geographical-coverage/grouped-locations-dialog.ts
+++ intervention-strategy/geographical-coverage/grouped-locations-dialog.ts
@@ -85,8 +85,8 @@ export class GroupedLocationsDialog extends LitElement {
           .selected="${this.adminLevel}"
           placeholder="&#8212;"
           .options="${this.adminLevels}"
-          option-label="admin_level_name"
-          option-value="admin_level_name"
+          option-label="name"
+          option-value="name"
           trigger-value-change-event
           @etools-selected-item-changed="${this.adminLevelChanged}"
         >
@@ -185,7 +185,7 @@ export class GroupedLocationsDialog extends LitElement {
       return [];
     }
     const index = locationTypes.findIndex(function (al: any) {
-      return al.admin_level_name === 'Country';
+      return al.name === 'Country';
     });
     if (index > -1) {
       locationTypes.splice(index, 1);
@@ -215,7 +215,7 @@ export class GroupedLocationsDialog extends LitElement {
   }
 
   adminLevelChanged(event: CustomEvent) {
-    const selectedAdminLevelName = get(event.detail, 'selectedItem.admin_level_name');
+    const selectedAdminLevelName = get(event.detail, 'selectedItem.name');
     if (!selectedAdminLevelName) {
       this.groupedLocations = [];
       return;
